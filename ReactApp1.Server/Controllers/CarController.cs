1201ad31597ad867d988caece31c4c912a48e563
boddal21 <150516401+boddal21@users.noreply.github.com>
2024-03-30 23:23:30 +0100

    CarController.cs frissítve

--- ReactApp1.Server/Controllers/CarController.cs
+++ ReactApp1.Server/Controllers/CarController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using ReactApp1.Server.DataContext.Model;
 using ReactApp1.Server.Services;
@@ -9,17 +9,34 @@ namespace ReactApp1.Server.Controllers
     [Route("api/[controller]/[action]")] //A [controller] kiveszi a class nevéből a "controller"-t, az [action] helyére pedig az adott függvény fog bekerülni
     public class CarController : ControllerBase
     {
-        private readonly ICarService _carService;
+        //private readonly ICarService _carService;
 
-        public CarController(ICarService carService)
-        {
-            _carService = carService;
-        }
+        //public CarController(ICarService carService)
+        //{
+        //    _carService = carService;
+        //}
 
         [HttpGet]
         public async Task<IActionResult> ListCars()
         {
-            List<CarModel> cars = await _carService.ListCars();
+            //List<CarModel> cars = await _carService.ListCars();
+            List<CarModel> cars = new List<CarModel>();
+            CarModel car1=new CarModel();
+            CarModel car2=new CarModel();
+            car1.Id = "c1";
+            car1.CategoryId = "1";
+            car1.Brand = "Toyota";
+            car1.Model = "Carmy";
+            car1.DailyPrice = "50$";
+
+            car2.Id = "c2";
+            car2.CategoryId = "2";
+            car2.Brand = "Honda";
+            car2.Model = "Civic";
+            car2.DailyPrice = "45$";
+
+            cars.Add(car1);
+            cars.Add(car2);
 
             return Ok(cars);
         }
